ab2b882848272b85f7045297e2d7a9d7e133163d
tuyo <tuyo@localhost.localdomain>
2016-04-30 21:06:54 +0900

    title change

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ for(key in rows){
  console.log(key);
 }           
 
-res.render('pages/users', { title: 'Express Users', users: rows.rows });
+res.render('pages/users', { title: 'heroku Express Users', users: rows.rows });
         });
     });
 });
